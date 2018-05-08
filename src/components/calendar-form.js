cc09eac40c2f6431d6bb72de021c97331613c4d5
Prabeen04 <prabeen.strange@gmail.com>
2018-05-08 20:34:30 +0530

    calendar and calendar form component craeted

---
+++ src/components/calendar-form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CalendarForm extends Component {
+  render() {
+    return (
+      <div>
+        <h2>CalendarForm</h2>
+      </div>
+    )
+  }
+}
+
+export default CalendarForm
