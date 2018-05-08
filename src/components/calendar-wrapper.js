ab68a639d44b47c9e4c482ffd631eac070c45e65
Prabeen04 <prabeen.strange@gmail.com>
2018-05-08 20:40:53 +0530

    events fetched from api

--- src/components/calendar-wrapper.js
+++ src/components/calendar-wrapper.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Calendar from './calendar';
+import CalendarForm from './calendar-form';
+import axios from 'axios';
+const baseUrl = 'https://prabeen-restapi.herokuapp.com/api/events';
 
 class CalendarWrapper extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Fullcalendar goes here</h1>
-      </div>
-    )
-  }
+    componentDidMount() {
+        axios.get(baseUrl)
+            .then((res) => {
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    render() {
+        return (
+            <div>
+                <h1>Fullcalendar goes here</h1>
+                <Calendar />
+                <CalendarForm />
+            </div>
+        )
+    }
 }
 
 export default CalendarWrapper;
