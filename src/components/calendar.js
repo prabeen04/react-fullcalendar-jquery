a9c2d20bd773421ba9dfc80cfe341821549a5cd4
Prabeen04 <prabeen.strange@gmail.com>
2018-05-08 21:34:49 +0530

    ref created in calendar

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+import 'fullcalendar/dist/fullcalendar.css';
+import 'fullcalendar/dist/fullcalendar.print.min.css';
+import 'fullcalendar/dist/fullcalendar.js';
 
 class Calendar extends Component {
   constructor(props){
     super(props)
-
+    this.calendarRef = React.createRef();
     this.renderEvents = this.renderEvents.bind(this);
   }
   renderEvents = () => {
@@ -11,11 +15,16 @@ class Calendar extends Component {
         return <p key={index}>{event.title}</p>
     })
   }
+  componentDidMount(){
+    $(this.calendarRef).fullCalendar()
+  }
   render() {
+    console.log(this.calendarRef)
     return (
       <div>
         <h2>Calendar</h2>
-        {this.renderEvents()}
+        <div ref={this.calendarRef}></div>
+        {/* {this.renderEvents()} */}
       </div>
     )
   }
